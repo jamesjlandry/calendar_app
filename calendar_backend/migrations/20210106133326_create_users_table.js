7367a079ee6210cda3df01a5966d5f575559613e
jamesjlandry <james.j.landry@outlook.com>
2021-01-06 13:40:03 -0600

    added table data to migrations

--- calendar_backend/migrations/20210106133326_create_users_table.js
+++ calendar_backend/migrations/20210106133326_create_users_table.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('users', function (table) {
+        table.increments();
+        table.string('name');
+        table.string('password')
+        table.string('email')
+        table.timestamps();
+      })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('users')
 };
--- calendar_backend/migrations/20210106133407_create_events_table.js
+++ calendar_backend/migrations/20210106133407_create_events_table.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('events', function (table) {
+        table.increments();
+        table.string('name');
+        table.string('description')
+        table.string('start')
+        table.string('duration')
+        table.boolean('complete')
+        table.integer('user_id')
+        table.timestamps();
+      })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('events')
 };
